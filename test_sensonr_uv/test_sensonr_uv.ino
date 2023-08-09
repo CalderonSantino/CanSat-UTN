ffeb5a9d1abe80716f9a2e5c3cd739c250dd32e5
Santino Calderon <contacto.santinocalderon@gmail.com>
2023-08-09 18:42:32 -0300

    Subiendo recursos

--- test_sensonr_uv/test_sensonr_uv.ino
+++ test_sensonr_uv/test_sensonr_uv.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+float sensorVoltage;
+float sensorValue;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(A1, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  sensorValue = analogRead(A1);
+  sensorVoltage = sensorValue / 1024 * 5.0;
+  Serial.print("sensor reading = ");
+  Serial.print(sensorValue);
+  Serial.print(" sensor voltage = ");
+  Serial.print(sensorVoltage);
+  Serial.println(" V");
+  delay(1000);
 }
